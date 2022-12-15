cc1a2c419e907d7bb55026fa24b1d6c698839e0d
lukascobit <lukyn.odehnal333@gmail.com>
2022-12-15 21:32:36 +0100

    the images moving

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,9 +1,7 @@
-import { Button, StyleSheet } from "react-native";
+import { Button, StyleSheet, Image } from "react-native";
 
-import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View } from "../components/Themed";
 import { RootTabScreenProps } from "../types";
-import { ScrollView } from "react-native";
 import { useSwipe } from "../hooks/useSwipe";
 import { useState } from "react";
 
@@ -12,13 +10,23 @@ export default function TabOneScreen({
 }: RootTabScreenProps<"TabOne">) {
 	const [mes, setMes] = useState("");
 	const { onTouchStart, onTouchEnd } = useSwipe(onSwipeDown, onSwipeUp, 6);
+	const [top, setTop] = useState(0);
+	const [bottom, setBottom] = useState(0);
 
-	function onSwipeDown() {
-		setMes("down");
-	}
 	function onSwipeUp() {
-		setMes("up");
+		setBottom(0);
+		setTop(top - 50);
+		start(false);
 	}
+	function onSwipeDown() {
+		setTop(0);
+		setBottom(bottom - 50);
+		start(true);
+	}
+
+	const start = (direction: boolean) => {
+		setMes(`Chose ${direction ? "up" : "down"}`);
+	};
 
 	return (
 		<View
@@ -26,21 +34,29 @@ export default function TabOneScreen({
 			onTouchEnd={onTouchEnd}
 			style={styles.container}
 		>
-			<Text style={styles.title}>Swipechoose</Text>
+			<Image
+				style={{ top: top, height: 500, width: 500 }}
+				source={require("../assets/images/tests/4k_phozo.jpg")}
+			/>
 			<Text style={styles.basicText}>
 				Swipe to choose which picture you like the better
 			</Text>
-			<Button
-				color={"red"}
-				title="say hey"
-				onPress={() => setMes(mes + "hey\n")}
-			></Button>
 			<Text>{mes}</Text>
+			<Image
+				style={{ top: top, height: 500, width: 500 }}
+				source={require("../assets/images/tests/big_snail.jpg")}
+			/>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
+	first: {
+		opacity: 1,
+	},
+	second: {
+		opacity: 1,
+	},
 	container: {
 		// position: "absolute",
 		// top: 0,
